a46acd24e8f166208576683e96a13ff64219aa00
Huong <huongnguyenak96@gmail.com>
2019-05-04 18:10:50 +0700

    for

---
+++ index02.php
@@ -0,0 +1,23 @@
+<?php
+
+// define('PI', 3.14);
+// echo PI;
+
+for($i=0; $i<=10; $i++){
+    if($i%2 === 0){
+        echo $i."<br>";
+    }
+}
+// echo "i = ";
+// echo  $i; // 11
+
+
+// $a = 1; // gán
+
+
+
+// $b = '1';
+// var_dump('1' == 1); // true
+// var_dump('1' === 1); //false: so sanh tuyet doi
+
+?>
