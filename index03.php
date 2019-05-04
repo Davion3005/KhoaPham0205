724d0364790b4c0ec3c567422c31f0f757031d4b
Huong <huongnguyenak96@gmail.com>
2019-05-04 18:56:10 +0700

    style

---
+++ index03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ban co vua</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <div class="banco">
+        <div class="oco trang">1</div>
+        <div class="oco den">2</div>
+        <div class="oco trang">3</div>
+        <div class="oco den">4</div>
+        <div class="oco trang"></div>
+        <div class="oco den"></div>
+        <div class="oco trang"></div>
+        <div class="oco den">8</div>
+    </div>
+</body>
+</html>
