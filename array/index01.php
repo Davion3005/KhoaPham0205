897ceb77066e223dbc442d9c2f8578cb6c4fb92e
Huong <huongnguyenak96@gmail.com>
2019-05-07 18:08:38 +0700

    array

---
+++ array/index01.php
@@ -0,0 +1,24 @@
+<?php
+$person = [
+    'name' => 'KPT',
+    'age' => 2,
+    4 => 123456,
+    true
+];
+$b = array(
+    'key1'=>1, 
+    'key2'=>2
+);
+
+$subject = array('PHP','NodeJS');
+$a = [1,2,3];
+
+var_dump($person);
+// echo "<pre>";
+print_r($person);
+// echo $person['name'];
+print_r($subject[1]);
+
+
+
+?>
