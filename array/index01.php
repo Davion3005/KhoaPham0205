4def65c6e929c9078bd3faeefd83435dc52f2c86
Huong <huongnguyenak96@gmail.com>
2019-05-07 18:30:33 +0700

    array

--- array/index01.php
+++ array/index01.php
@@ -7,17 +7,39 @@ $person = [
 ];
 $b = array(
     'key1'=>1, 
-    'key2'=>2
+    'key2'=>2,
+    $person
 );
+// print_r($b);
+// echo $b[0][4];
 
-$subject = array('PHP','NodeJS');
-$a = [1,2,3];
+// $subject = array('PHP','NodeJS',$b);
 
-var_dump($person);
-// echo "<pre>";
-print_r($person);
-// echo $person['name'];
-print_r($subject[1]);
+// echo $subject[2][0][4];
+
+// var_dump($person);
+// // echo "<pre>";
+// print_r($person);
+// // echo $person['name'];
+// print_r($subject[1]);
+
+
+$a = [1,2,3,5,88,3,1,4,5,7,4,3];
+
+foreach($a as $number) echo $number.'<br>';
+
+// // i=0
+// $number = 1;
+// // i = 1;
+// $number = 2;
+
+// for($i=0; $i < 12; $i++){
+//     echo $a[$i]."<br>";
+// }
+
+// echo $a[0];
+// echo $a[1];
+// echo $a[2];
 
 
 
---
+++ array/index02.php
@@ -0,0 +1,8 @@
+<?php
+$person = [
+    'name' => 'KPT',
+    'age' => 2,
+    4 => 123456,
+    true
+];
+
