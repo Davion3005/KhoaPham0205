d98f02b99f77e9a9e4b0bdcf38e4667c9bc26881
Huong <huongnguyenak96@gmail.com>
2019-05-18 19:29:39 +0700

    check login

--- form/login/home.php
+++ form/login/home.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+
+// get token from SESSION to get userID 
+// request to db get user info
+if(!isset($_SESSION['user']) || $_SESSION['user'] !== md5(md5(121313))) {
+    $_SESSION['error'] = "Please login!";
+    header('Location: index.php');
+    return false;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
