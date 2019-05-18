736e56e053f61fe983298a88b6d6554690417ce1
Huong <huongnguyenak96@gmail.com>
2019-05-18 19:15:05 +0700

    isset

--- form/login/index.php
+++ form/login/index.php
@@ -14,7 +14,7 @@
             <h2 class="text-center">Login</h2>
             <?php
             if(isset($_SESSION['error'])){
-                echo $_SESSION['error'];
+                echo '<div class="alert alert-danger">'.$_SESSION['error']."</div>";
                 unset($_SESSION['error']);
             }
             ?>
--- form/login/xulylogin.php
+++ form/login/xulylogin.php
@@ -1,8 +1,17 @@
 <?php
 session_start();
-$email = $_POST['txtEmail'];
-$password = $_POST['txtPassword'];
+$email = isset($_POST['txtEmail']) ? $_POST['txtEmail'] : null;
+$password = isset($_POST['txtPassword']) ? $_POST['txtPassword'] : null;
+if($email == null || $password == null){
+    $_SESSION['error'] = "Please login!";
+    header('location: index.php');
+    return;
+}
+
+// request db to get user info
 if($email=='admin@gmail.com' && $password=='111111'){
+    $userId = 121313; // get from db
+    $_SESSION['user'] = md5(md5($userId));
     header('location: home.php');
 }
 else{
