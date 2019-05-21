74285833d93a828eed64e35b07ebd48b5c07bb88
Huong <huongnguyenak96@gmail.com>
2019-05-21 19:29:29 +0700

    parameters

--- function/index.php
+++ function/index.php
@@ -2,17 +2,28 @@
 
 /**
  * print number
+ * @param integer $a
  * @return integer
  */
-function inSo(){
-    return 1;
+function inSo(int $a, int $b=0): int{
+    $a+=$b; // a = a+b
+    return $a;
 }
+echo inSo(2, 5);
 
 // echo inSo();
 
 // $kq = inSo() + 2; 
 // echo $kq;  // 3
 
-number_format(2000);
+// echo number_format(2222);
+
+/**
+ * Viet funtion thuc hien cac chuc nang sau:
+ * - min: 0, max: 20;
+ * - in ra day cac so chia het cho 2
+ * - in ra day cac so ko chia het cho 2
+ * - in ra day cac so chia het cho 4 du 3
+ */
 
 ?>
