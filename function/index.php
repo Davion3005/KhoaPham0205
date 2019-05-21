86796527c4f30bacd64e59d4237e3a5d661bffc5
Huong <huongnguyenak96@gmail.com>
2019-05-21 19:07:11 +0700

    function

---
+++ function/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * print number
+ * @return integer
+ */
+function inSo(){
+    return 1;
+}
+
+// echo inSo();
+
+// $kq = inSo() + 2; 
+// echo $kq;  // 3
+
+number_format(2000);
+
+?>
