835bcc3062a00984da65b4d883d82c08981f2e24
Huong <huongnguyenak96@gmail.com>
2019-05-23 18:07:25 +0700

    checkSNT

--- function/index03.php
+++ function/index03.php
@@ -1,10 +1,20 @@
 <?php
 /**
  * Cho day cac so tu min=0 -> max=100;
- * Đếm xem trong dãy số trên, có bao nhiên số nguyên tố,
+ * Đếm xem trong dãy số trên có bao nhiêu số nguyên tố,
  * In ra dãy các số ngtố đó.
  * 2 3 5 7 11 13 17 ...
  */
  
+function checkSNT(int $number): bool{
+    if($number == 0 || $number == 1)
+        return false;
+    for($i=2; $i <= sqrt($number); $i++){
+        if($number%$i==0) return false;
+    }
+    return true;
+}
+// $c = checkSNT(9);
+// var_dump($c);
 
 ?>
