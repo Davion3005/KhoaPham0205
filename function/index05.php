3e739fc83940d0c1cd706f15757fe03d2ea6714c
Huong <huongnguyenak96@gmail.com>
2019-05-23 19:34:20 +0700

    buildin function

---
+++ function/index05.php
@@ -0,0 +1,18 @@
+<?php
+// $str = 'Hôm nay Nam đi học';
+// $arrStr = explode(' ',$str);
+
+// $newStr = implode('-',$arrStr);
+// echo $newStr;
+// echo strlen('Đi học'); //9 
+
+$str='qwertyuiopasdfghjklzxcvbnm0987654321';
+// echo $str[0];
+// echo rand(0,99);
+/**
+ * Viet function random string voi độ dài do user truyền vào
+ */
+
+ //adcaec380a912c0d0b14a81bedb6ff
+
+?>
