610287f248cb81383326aa86480f1c6f3e6c3be4
Huong <huongnguyenak96@gmail.com>
2019-05-25 19:03:39 +0700

    check

--- file/xulyupload.php
+++ file/xulyupload.php
@@ -1,17 +1,28 @@
 <?php
 $avatar = $_FILES['avatar']; // return array
-print_r($avatar);
-
+// print_r($avatar);
 /**
  * check file type ( image/png, image/gif, image/jpeg)
- * check file size <= 1 MB => 1024*1024  
+ * check file size <= 100kb => 100*1024  
  * rename file
  */
 
-if($avatar['type'] == 'image/png'){
-    
-    move_uploaded_file($avatar['tmp_name'],'avatar-user/'.$avatar['name']);
-    echo 'uploaded';
+
+$arrMimeType = [
+    'image/png',
+    'image/gif',
+    'image/jpeg'
+];
+if(in_array($avatar['type'], $arrMimeType)){
+
+    if( $avatar['size'] <= 100*1024 ){ //100kb
+        move_uploaded_file($avatar['tmp_name'],'avatar-user/'.$avatar['name']);
+        echo 'uploaded';
+    }
+    else{
+        echo 'File too large!';
+        return;
+    }
 }
 else{
     echo 'File not allow!';
