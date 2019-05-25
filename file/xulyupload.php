4e948fd099482d01049e2bc50b2b10161cbde646
Huong <huongnguyenak96@gmail.com>
2019-05-25 18:49:02 +0700

    upload file

--- file/xulyupload.php
+++ file/xulyupload.php
@@ -1,6 +1,23 @@
 <?php
 $avatar = $_FILES['avatar']; // return array
-echo $name = $_POST['txtName']; // string
 print_r($avatar);
+
+/**
+ * check file type ( image/png, image/gif, image/jpeg)
+ * check file size <= 1 MB => 1024*1024  
+ * rename file
+ */
+
+if($avatar['type'] == 'image/png'){
+    
+    move_uploaded_file($avatar['tmp_name'],'avatar-user/'.$avatar['name']);
+    echo 'uploaded';
+}
+else{
+    echo 'File not allow!';
+    return;
+}
+
+
 
 ?>
