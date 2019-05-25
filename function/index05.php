e6fb19ae44534f942db84f7a3050121cc6d0700b
Huong <huongnguyenak96@gmail.com>
2019-05-25 17:52:14 +0700

    randomString

--- function/index05.php
+++ function/index05.php
@@ -4,15 +4,29 @@
 
 // $newStr = implode('-',$arrStr);
 // echo $newStr;
-// echo strlen('Đi học'); //9 
+// echo strlen('Đi học'); // 9 
 
-$str='qwertyuiopasdfghjklzxcvbnm0987654321';
+// $str='qwertyuiopasdfghjklzxcvbnm0987654321';
 // echo $str[0];
 // echo rand(0,99);
-/**
- * Viet function random string voi độ dài do user truyền vào
- */
 
- //adcaec380a912c0d0b14a81bedb6ff
+/**
+ * function random string voi độ dài do user truyền vào
+ */
+function randomString(int $length = 40){
+    $initString='qwertyuiopasdfghjklzxcvbnm0987654321';
+    $str = '';
+    for($i=1; $i <= $length; $i++){
+        $position = rand(0, strlen($initString)-1);
+        // $str .= $initString[$position];
+        $str .= substr($initString,$position,1);
+    }
+    return $str;
+}
+echo randomString(30);
+//x3bdi9dvkqyb8hdjsnjvvzwzfxwyw92omdkkw6nk
+//1vtnq08vv7xvhp9gzakbtvsdcyvc0xilupg5kzq4
+//z86op608zga0n5lpa8bjg898lajwwn
+//u69ifjdbcgjtksmw05668ws19jn18g
 
 ?>
