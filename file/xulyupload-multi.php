63fd3bc9a284755071df9293f78f04290756c6f4
Huong <huongnguyenak96@gmail.com>
2019-05-25 19:34:55 +0700

    upload multi

--- file/xulyupload-multi.php
+++ file/xulyupload-multi.php
@@ -5,7 +5,7 @@ $files = $_FILES['avatar'];
 /**
  * check file type (image/png, image/gif, image/jpeg)
  * check file size <= 100kb => 100*1024  
- * 
+ * if 1 file is error => rollback all file upload
  */
 foreach($files['tmp_name'] as $key=>$tmpName){
     $newName = time().'-'.$files['name'][$key];
