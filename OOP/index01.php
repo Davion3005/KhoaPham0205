29610bb9fee9f026d584300eb98c12bf4e0cd176
Huong <huongnguyenak96@gmail.com>
2019-05-28 18:10:27 +0700

    obj

---
+++ OOP/index01.php
@@ -0,0 +1,19 @@
+<?php
+class Product{
+    public $name = 'IPhone';
+    public $price;
+}
+
+$product = new Product; // obj
+// echo $product->name;
+print_r($product);
+
+// $product02 = [
+//     'name'=>'Mac',
+//     'price'=>null
+// ];
+// echo $product02['name'];
+// print_r($product02);
+
+
+?>
