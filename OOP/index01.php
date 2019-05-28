57ce0fb7d36d5eb4fb00101a37148b6a8fd7e951
Huong <huongnguyenak96@gmail.com>
2019-05-28 18:30:57 +0700

    function

--- OOP/index01.php
+++ OOP/index01.php
@@ -1,12 +1,36 @@
 <?php
 class Product{
-    public $name = 'IPhone';
-    public $price;
+    //property
+    protected $id;
+    protected $name = 'iPhone';
+    protected $price;
+    protected $desc;
+    protected $image;
+
+    function __construct()
+    {
+        echo __CLASS__ . __FUNCTION__ . ' duoc goi';
+    }
+
+    // function 
+    function setName(string $txtName){
+        $this->name = $txtName;
+    }
+    function getName(){
+        return $this->name;
+    }
+
 }
 
 $product = new Product; // obj
-// echo $product->name;
-print_r($product);
+// $product->__construct();
+
+// $product->name = 'Macbook';
+// $product->setName('Samsung');
+// echo $product->getName();
+// print_r($product);
+
+
 
 // $product02 = [
 //     'name'=>'Mac',
